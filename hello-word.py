1ab46a9476fd820082f7f3c234466678adefa696
EC2 Default User <ec2-user@ip-10-16-10-121.us-west-2.compute.internal>
2022-06-22 02:15:36 +0000

    archivos para ejercicio python

--- hello-word.py
+++ hello-word.py
@@ -1,2 +1,8 @@
 pedir = input("Ingresa tu nombre: ")
 print("Tu nombre es: " + pedir)
+
+pedir = input("Ingresa tu nombre: ")
+print("Tu nombre es: " + pedir)
+
+pedir = input("Ingresa tu nombre: ")
+print("Tu nombre es: " + pedir)
